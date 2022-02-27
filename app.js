6a599d023826704ebe2ebfbebaec70e6b9bbbac3
anhy167 <ttka1607@gmail.com>
2022-02-28 01:21:48 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const setupContactRoutes = require("./app/routes/contact.routes");
+const { BadRequestError, errorHandler } = require("./app/errors");
+
 
 const app = express();
 
@@ -12,5 +14,13 @@ app.get("/",(req, res) => {
 });
 
 setupContactRoutes(app);
+
+app.use((req, res, next) => {
+    next(new BadRequestError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    errorHandler.handleError(err, res);
+});
 
 module.exports = app;
